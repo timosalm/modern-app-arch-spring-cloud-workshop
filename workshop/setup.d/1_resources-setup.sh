f37ddbcae1cf6871264e98b5cb9f70733b75ef5c
Timo Salm <tsalm@vmware.com>
2023-07-25 08:50:11 +0200

    Initial commit

--- workshop/setup.d/1_resources-setup.sh
+++ workshop/setup.d/1_resources-setup.sh
@@ -2,7 +2,5 @@
 set -x
 set +e
 
-kubectl patch serviceaccount default -p '{"secrets": [{"name": "registry-credentials"}], "imagePullSecrets": [{"name": "registry-credentials"}]}'
-
 cp -a samples/spring-cloud-demo/. .
 rm -rf samples/spring-cloud-demo
