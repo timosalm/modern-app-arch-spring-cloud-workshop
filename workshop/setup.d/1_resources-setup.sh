cbacd1d0e722a0d1b7f7ff597b8342f6e7b5aa35
Timo Salm <tsalm@vmware.com>
2023-07-25 10:05:37 +0200

    Initial commit

--- workshop/setup.d/1_resources-setup.sh
+++ workshop/setup.d/1_resources-setup.sh
@@ -2,5 +2,9 @@
 set -x
 set +e
 
+kubectl annotate namespace ${SESSION_NAMESPACE} secretgen.carvel.dev/excluded-from-wildcard-matching-
+kubectl label namespaces ${SESSION_NAMESPACE} apps.tanzu.vmware.com/tap-ns=""
+kubectl patch serviceaccount default -p '{"secrets": [{"name": "registry-credentials"},{"name": "git-https"}], "imagePullSecrets": [{"name": "registry-credentials"}]}'
+
 cp -a samples/spring-cloud-demo/. .
 rm -rf samples/spring-cloud-demo
