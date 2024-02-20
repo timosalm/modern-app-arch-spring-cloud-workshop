8b55e1f5c95a15cd60da1d2437a095e592fb5d7a
Timo Salm <52704922+timosalm@users.noreply.github.com>
2024-02-20 15:18:22 +0100

    Improvements

--- workshop/setup.d/2_terminal-setup.sh
+++ workshop/setup.d/2_terminal-setup.sh
@@ -21,12 +21,12 @@ set +e
 
 (cd /opt/git/repositories && git init && git config --global --add safe.directory /opt/git/repositories && git instaweb)
 
-for serviceName in order-service shipping-service product-service; do
-    sed -i 's~REGISTRY_HOST~'"$REGISTRY_HOST"'~g' samples/spring-cloud-demo/${serviceName}/config/workload.yaml
-done
-
 mv samples/spring-cloud-demo/order-service .
 mv samples/spring-cloud-demo/shipping-service .
 mv samples/spring-cloud-demo/product-service .
 
-(cd ~/samples/externalized-configuration && sed -i 's~NAMESPACE~'"$SESSION_NAMESPACE"'~g' order-service.yaml)
+for serviceName in order-service shipping-service product-service; do
+    (cd ${serviceName} && git init -b main && git remote add origin {{ git_protocol }}://{{ git_host}}/${serviceName}.git && git add . && git commit -m "Initial implementation" && git push -u origin main)
+done
+
+(cd ~/samples/externalized-configuration && sed -i 's~NAMESPACE~'"$SESSION_NAMESPACE"'~g' order-service.yaml && git init -b main && git remote add origin {{ git_protocol }}://{{ git_host}}/externalized-configuration.git && git add . && git commit -m "Initial implementation" && git push -u origin main)
